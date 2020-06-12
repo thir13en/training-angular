0a69e8af5fb5bc76534dced6aa5a272c81f8b8e4
thir13en <santilpzate@gmail.com>
2020-06-12 14:53:10 +0200

    added service example

---
+++ src/app/services/example.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExampleService {
+
+  constructor(private http: HttpClient) { }
+
+  getWhatever() {
+    const params = new HttpParams()
+      .set('page', 1)
+      .set('pageSize', 10)
+
+    return this.http.get('/whatever/', { params });
+  }
+}
