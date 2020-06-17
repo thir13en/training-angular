92876d42c383cbeff98009c080ad5f3e172e82dd
thir13en <santilpzate@gmail.com>
2020-06-17 22:00:54 +0200

    replaced directives

--- src/app/features/directive-demo/directive-demo.module.ts
+++ src/app/features/directive-demo/directive-demo.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DirectiveDemoRoutingModule } from './directive-demo-routing.module';
-import { HighlightDirective } from '../../directives/highlight.directive';
+import { HighlightDirective } from './directives/highlight.directive';
 import { DirectiveExamplesComponent } from './components/directive-examples/directive-examples.component';
 
 
