2865eea8c56d80d729d94d5ebaafac29e540666f
thir13en <santilpzate@gmail.com>
2020-06-19 02:06:12 +0200

    thousands of insights in directives

--- src/app/features/directives-demo/components/directives-examples/directives-examples.component.ts
+++ src/app/features/directives-demo/components/directives-examples/directives-examples.component.ts
@@ -1,28 +1,25 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { HighlightDirective } from '../../directives/highlight.directive';
+import { DirectivesDummyComponent } from '../directives-dummy/directives-dummy.component';
 
 @Component({
   templateUrl: './directives-examples.component.html',
   styleUrls: ['./directives-examples.component.scss']
 })
-export class DirectivesExamplesComponent implements OnInit {
-  @Input()
-  private isHighlighted = false;
+export class DirectivesExamplesComponent implements AfterViewInit {
+  // getting a reference to a directive
+  @ViewChild(HighlightDirective)
+  highlightDirective!: HighlightDirective;
+
+  // another way to express it
+  @ViewChild(DirectivesDummyComponent, {read: HighlightDirective})
+  highlightDirective2!: HighlightDirective;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  // you need to apply a typescript getter method in order for this host binding to work
-  // this adds always a classname highlighted
-  // @HostBinding('className')
-  // get cssClasses() {
-  //   return 'highlighted';
-  // }
-
-  @HostBinding('class.highlighted')
-  get cssClasses() {
-    return this.isHighlighted;
+  ngAfterViewInit(): void {
+    console.log(this.highlightDirective);
+    console.log(this.highlightDirective2);
   }
 
 }
--- src/app/features/directives-demo/directives/highlight.directive.ts
+++ src/app/features/directives-demo/directives/highlight.directive.ts
@@ -1,13 +1,50 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
 
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[highlight]',
+  exportAs: 'hl',
 })
 export class HighlightDirective {
+  @Input('highlight')
+  private isHighlighted = false;
+
+  // directives can also emit events
+  @Output()
+  highlightChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor() {
     console.log('works');
+  }
+
+  // you need to apply a typescript getter method in order for this host binding to work
+  // this adds always a classname highlighted
+  // @HostBinding('className')
+  // get cssClasses() {
+  //   return 'highlighted';
+  // }
+
+  @HostBinding('class.highlighted')
+  get cssClasses() {
+    return this.isHighlighted;
+  }
+
+  // to write to native html attributes
+  @HostBinding('attr.disabled')
+  get isDisabled() {
+    return true;
+  }
+
+  @HostListener('click', ['$event'])
+  handleClick($event: MouseEvent) {
+    alert('click');
+    console.log($event);
+    this.toggle();
+  }
+
+  // lets suppose that this method is thought out to be used for third party components
+  toggle() {
+    this.isHighlighted = !this.isHighlighted;
   }
 
 }
