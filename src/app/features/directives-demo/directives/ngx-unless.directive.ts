a40e06949f5487a6ebc40916b39bc5d3c950fc03
thir13en <santilpzate@gmail.com>
2020-06-20 02:06:12 +0200

    finished adding directive examples

--- src/app/features/directives-demo/directives/ngx-unless.directive.ts
+++ src/app/features/directives-demo/directives/ngx-unless.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
 
 @Directive({
-  selector: '[appNgxUnless]'
+  selector: '[ngxUnless]'
 })
 export class NgxUnlessDirective {
 
-  constructor() { }
+  private visible = false;
+
+  constructor(
+    // the manipulation of these guys is the key to structural directives creation
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef,
+  ) { }
+
+  @Input()
+  set ngxUnless(condition: boolean) {
+    if (!(condition || this.visible)) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainerRef.clear();
+      this.visible = true;
+    }
+  }
 
 }
