01575750337af1865f42f9576da7eea911916738
thir13en <santilpzate@gmail.com>
2020-07-01 16:34:40 +0200

    ended pipes section

--- src/app/pipes/filter-example.pipe.ts
+++ src/app/pipes/filter-example.pipe.ts
@@ -4,7 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 // will filter by category in the following way:
 // arrayWithCategory | filterExample: 'CATEGORY_TO_FILTER'
 @Pipe({
-  name: 'filterExample'
+  name: 'filterExample',
+  // when input data of the pipe is mutated, it wont be triggered by default
+  // to make it reactive to input data mutations, you should make it unpure,
+  // which will make the pipe trigger with every change detection cycle.
+  pure: false,
+  // with this pipe will run with every life cycle
 })
 export class FilterExamplePipe implements PipeTransform {
 
