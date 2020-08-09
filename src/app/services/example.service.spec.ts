fb74a1fb1cd9aa185f46eb250efdde474e39b229
santiloopz <santilpzate@gmail.com>
2020-08-09 09:37:23 +0200

    fixing test suite

--- src/app/services/example.service.spec.ts
+++ src/app/services/example.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
+import { TestingModule } from '@testing/testing.module';
 import { ExampleService } from './example.service';
+
 
 describe('ExampleService', () => {
   let service: ExampleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TestingModule],
+    });
     service = TestBed.inject(ExampleService);
   });
 
