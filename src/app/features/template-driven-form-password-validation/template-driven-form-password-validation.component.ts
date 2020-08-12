ccb879e41ac104ce9b4f84d1f121c8a24636a6df
santiloopz <santilpzate@gmail.com>
2020-08-12 09:07:44 +0200

    filled template driven form structure

--- src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
+++ src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
@@ -4,7 +4,30 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   template: `
     <section id="form-password-validation">
-      <form #f="ngForm">
+      <form #f="ngForm" autocomplete="off" novalidate>
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name">
+          <p class="error">This field is mandatory</p>
+        </div>
+
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email">
+          <p class="error">This field is mandatory</p>
+        </div>
+
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password">
+        </div>
+
+        <div>
+          <label for="confirm">Confirm password:</label>
+          <input type="password" id="confirm" name="confirm">
+        </div>
+
+        <button type="submit">Sign up!</button>
 
       </form>
     </section>
