0fc95bff2f66d109f4e76a5fbf60374a75b6d9bd
santiloopz <santilpzate@gmail.com>
2020-08-16 14:44:07 +0200

    refactor color scss theming architecture

--- notes/index.md
+++ notes/index.md
@@ -1,6 +1,9 @@
 # Index
 
-1. [Forms](./forms/index.md)
-1. [Dependency Injection](./dependency_injection.md)
-1. [Directives](./directives.md)
-1. [Angular Elements](./angular_elements.md)
+1. [Angular Elements](angular-elements.md)
+1. [Change Detection](change-detection.md)
+1. [Components](components.md)
+1. [Forms](forms/index.md)
+1. [Dependency Injection](dependency_injection.md)
+1. [Directives](directives.md)
+1. [Angular Material](angular-material.md)
--- src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
+++ src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
@@ -3,34 +3,33 @@ import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 
 @Component({
   template: `
-    <section id="form-password-validation">
-      <form #f="ngForm" autocomplete="off" novalidate>
-        <div>
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name">
-          <p class="error">This field is mandatory</p>
-        </div>
+    <form #f="ngForm" autocomplete="off" novalidate>
 
-        <div>
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email">
-          <p class="error">This field is mandatory</p>
-        </div>
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+        <p class="error-message c-accent">This field is mandatory</p>
+      </div>
 
-        <div>
-          <label for="password">Password:</label>
-          <input type="password" id="password" name="password">
-        </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
+        <p class="error-message">This field is mandatory</p>
+      </div>
 
-        <div>
-          <label for="confirm">Confirm password:</label>
-          <input type="password" id="confirm" name="confirm">
-        </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+      </div>
 
-        <button type="submit">Sign up!</button>
+      <div>
+        <label for="confirm">Confirm password:</label>
+        <input type="password" id="confirm" name="confirm">
+      </div>
 
-      </form>
-    </section>
+      <button type="submit">Sign up!</button>
+
+    </form>
   `,
   styles: [`
 
