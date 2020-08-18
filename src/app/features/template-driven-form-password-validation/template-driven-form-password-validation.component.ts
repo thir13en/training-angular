d387b00e0f881c3489171428e8cc131e792a4d2b
santiloopz <santilpzate@gmail.com>
2020-08-18 09:37:59 +0200

    styling the form

--- src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
+++ src/app/features/template-driven-form-password-validation/template-driven-form-password-validation.component.ts
@@ -3,31 +3,47 @@ import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 
 @Component({
   template: `
+    <h1>Password Validation example</h1>
     <form #f="ngForm" autocomplete="off" novalidate>
 
-      <div class="d-flex justify-content-end">
+      <div class="d-flex justify-content-end mb-sm3">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" ngModel required>
-        <p class="error-message c-warn">This field is mandatory</p>
+        <p
+          *ngIf="f.controls.name.invalid && f.controls.name.touched"
+          class="error-message c-warn"
+        >This field is mandatory</p>
       </div>
 
-      <div>
+      <div class="d-flex justify-content-end mb-sm3">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" ngModel required>
-        <p class="error-message c-warn">This field is mandatory</p>
+        <p
+          *ngIf="f.controls.email.invalid && f.controls.email.touched"
+          class="error-message c-warn"
+        >This field is mandatory</p>
       </div>
 
-      <div>
+      <div class="d-flex justify-content-end mb-sm3">
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" ngModel required>
+        <p
+          *ngIf="f.controls.password.invalid && f.controls.password.touched"
+          class="error-message c-warn"
+        >This field is mandatory</p>
       </div>
 
-      <div>
+      <div class="d-flex justify-content-end mb-sm3">
         <label for="confirm">Confirm password:</label>
         <input type="password" id="confirm" name="confirm" ngModel required>
+        <p
+          *ngIf="f.controls.confirm.invalid && f.controls.confirm.touched"
+          class="error-message c-warn"
+        >This field is mandatory</p>
       </div>
-
-      <button type="submit" [disabled]="!f.touched || f.invalid">Sign up!{{f.invalid}}</button>
+      <div class="d-flex justify-content-end mb-sm3">
+        <button type="submit" [disabled]="!f.touched || f.invalid">Sign up!{{f.invalid}}</button>
+      </div>
 
     </form>
   `,
@@ -35,5 +51,5 @@ import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TemplateDrivenFormPasswordValidationComponent {
-  @HostBinding('class') classList = 'd-flex';
+  @HostBinding('class') classList = 'd-flex flex-column align-items-center w-100';
 }
