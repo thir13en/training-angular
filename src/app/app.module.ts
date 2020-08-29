a4968d20b74dcae62364822a0df97f170b4fd45f
santiloopz <santilpzate@gmail.com>
2020-08-29 09:24:09 +0200

    refactored folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DatePipe } from './pipes/date.pipe';
-import { FilterExamplePipe } from './pipes/filter-example.pipe';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from './shared/pipes/date.pipe';
+import { FilterExamplePipe } from './shared/pipes/filter-example.pipe';
 
 @NgModule({
   declarations: [
@@ -16,8 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
