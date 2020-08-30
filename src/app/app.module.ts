50f56251b38ed7e9f869a5f4b83fea3f47df0352
santiloopz <santilpzate@gmail.com>
2020-08-30 10:48:24 +0200

    reorganized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DatePipe } from './shared/pipes/date.pipe';
-import { FilterExamplePipe } from './shared/pipes/filter-example.pipe';
+import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MainLayoutComponent, ToggleHamburgerComponent } from '@layout/components';
+import { DatePipe } from '@shared/pipes/date.pipe';
+import { FilterExamplePipe } from '@shared/pipes/filter-example.pipe';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
