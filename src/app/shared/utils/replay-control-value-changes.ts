2ae817310e5205685b884b4680cfa9bf7bf5b9b7
santiloopz <santilpzate@gmail.com>
2020-10-27 08:39:48 +0100

    moved replay control value changes to utils

--- src/app/shared/utils/replay-control-value-changes.ts
+++ src/app/shared/utils/replay-control-value-changes.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AbstractControlDirective } from '@angular/forms';
 
-import { Observable, scheduled as observableScheduled } from 'rxjs';
+import { Observable } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 
 @Injectable({
@@ -14,9 +15,8 @@ export class ReplayControlValueChanges<T> extends Observable<T> {
         throw new Error('Control does not have valueChanges');
       }
 
-      // TODO: solve this
       control.valueChanges.pipe(
-        observableScheduled(control.value)
+        startWith(control.value)
       ).subscribe(subscriber);
     });
   }
