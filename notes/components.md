04c0b51641c253225fb6c6d884af9d875135e9c1
santiloopz <santilpzate@gmail.com>
2020-11-14 10:34:49 +0100

    preparing for updating to Angular 11

--- notes/components.md
+++ notes/components.md
@@ -10,3 +10,7 @@ Angular `Components` are directives with a template, don't forget that men! Comp
 With events, it's very easy to fall into the situation of using an event to trigger an action in an external component. The key thing to bear in mind about events, is that in order to keep the event emitter decoupled from the subscriber, **the emitter should only report about changes on its internal state**.
 
 ### TODO: learn about [component harness](https://indepth.dev/create-a-component-harness-for-your-tests-with-angular-cdk/)
+
+### Smart vs Presentational
+* **Smart** or **Container** do have business logic and know how to retrieve and distribute data.
+* **Presentational**, **pure** or **dummy** components just receive data as an input and paint it on the screen.
--- src/app/shared/directives/resize-observer-directive.directive.ts
+++ src/app/shared/directives/resize-observer-directive.directive.ts
@@ -1,8 +1,8 @@
 import { Directive, Inject, Output } from '@angular/core';
-
-import { ResizeObserverService } from '@shared/services';
 import { Attribute } from '@angular/compiler';
+
 import { Observable } from 'rxjs';
+import { ResizeObserverService } from '@shared/services';
 
 
 // TODO: create this provider https://medium.com/its-tinkoff/service-as-observable-29ce7fa9085b
@@ -15,18 +15,15 @@ function boxFactory() {}
     ResizeObserverService,
     {
       provide: RESIZE_OPTION_BOX,
-      // TODO: figure out this
       // deps: [[new Attribute('appResizeBox')]],
       useFactory: boxFactory,
     },
   ],
 })
 export class ResizeObserverDirective {
-  // @ts-ignore
   @Output() readonly waResizeObserver: Observable<ResizeObserverEntry[]>;
 
   constructor(
-    // @ts-ignore
     @Inject(ResizeObserverService) entries$: Observable<ResizeObserverEntry[]>
   ) {
     this.waResizeObserver = entries$;
